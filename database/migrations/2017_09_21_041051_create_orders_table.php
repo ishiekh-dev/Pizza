91100c51e768e4aecdb00a39aadc554854b2b738
ibrahem-20202020 <M.keshta@dp-itc.com>
2019-08-29 02:26:16 +0200

    First commit

---
+++ database/migrations/2017_09_21_041051_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('address')->nullable();
+            $table->string('size')->nullable();
+            $table->string('toppings')->nullable();
+            $table->string('instructions')->nullable();
+            $table->unsignedInteger('status_id');
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
+
+            $table->timestamps();
+
+             });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
